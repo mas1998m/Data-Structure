190a9c2ac7ef3ed649f90db1ac0e9847295f4aa7
Mohamed Abdullah <mas_seyam@yahoo.com>
2020-03-15 00:42:00 +0200

    added variables setters and getters

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -9,4 +9,14 @@ private:
 public:
 	Node():data(0), next(nullptr){}
 	Node(int d) :data(d), next(nullptr){}
+	void set_next(Node * ptr){
+		next = ptr;
+	}
+	Node * get_next(){
+		return next;
+	}
+	void set_data(int d){ data = d; }
+	int get_data(){ return data; }
+	friend LinkedList;
 };
+
